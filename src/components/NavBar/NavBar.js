6c8b0d8a1376ce0bb9e1c2325bf2ab58c8f1e40d
Alejandra Garcia <alejandragybra@gmail.com>
2024-01-24 19:27:37 -0300

    modificaciones en navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,20 +6,24 @@ import bookStoreLogo from './img/book-store-logo.png';
 
 const NavBar = () => {
     return (
-        <nav className='flex items-center justify-between shadow-lg bg-violet-600'>
-            <h1 className='hidden'>Book-store</h1>
-            <Link to = '/' className='flex justify-between items-center  pl-4 pr-4 pt-4 pb-4 rounded-r-3xl bg-white lg:pr-12 lg:pl-12'>
-                <img src={bookStoreLogo} alt='logo' className='h-6' />
-            </Link>
-            <div className='flex lg:w-10/12 h-14 pl-6 pr-6 items-center justify-between'>
-                <div className='hidden lg:flex gap-10 items-center'>
-                    <NavLink to={`/category/Ciencia`} className={({ isActive }) => isActive ? 'text-black' : 'text-white'}>Ciencia</NavLink>
-                    <NavLink to={`/category/Filosofía`} className={({ isActive }) => isActive ? 'text-black' : 'text-white'}>Filosofía</NavLink>
-                    <NavLink to={`/category/Narrativa`} className={({ isActive }) => isActive ? 'text-black' : 'text-white'}>Narrativa</NavLink>
-                    <NavLink to={`/category/Poesía`} className={({ isActive }) => isActive ? 'text-black' : 'text-white'}>Poesía</NavLink>
-                    <NavLink to={`/category/Psicología`} className={({ isActive }) => isActive ? 'text-black' : 'text-white'}>Psicología</NavLink>
-                </div>
+        <nav className='shadow-lg'>
+            <div className='flex items-center justify-between  shadow-lg bg-violet-600'>
+                <h1 className='hidden'>Book-store</h1>
+                <Link to='/' className='flex justify-between items-center  pl-4 pr-4 pt-4 pb-4 rounded-r-3xl bg-white lg:pr-12 lg:pl-12'>
+                    <img src={bookStoreLogo} alt='logo' className='h-6' />
+                </Link>
                 <CartWidget />
+            </div>
+
+            <div className='flex flex-col h-56 px-6 pb-6 gap-10 md:gap-5 md:h-32'>
+                <p className='font-bold text-violet-600 mt-2 '>Categorías</p>
+                <div className='flex flex-wrap gap-10 items-center'>
+                    <NavLink to={`/category/Ciencia`} className={({ isActive }) => isActive ? 'text-violet-600' : 'text-black'}>Ciencia</NavLink>
+                    <NavLink to={`/category/Filosofía`} className={({ isActive }) => isActive ? 'text-violet-600' : 'text-black'}>Filosofía</NavLink>
+                    <NavLink to={`/category/Narrativa`} className={({ isActive }) => isActive ? 'text-violet-600' : 'text-black'}>Narrativa</NavLink>
+                    <NavLink to={`/category/Poesía`} className={({ isActive }) => isActive ? 'text-violet-600' : 'text-black'}>Poesía</NavLink>
+                    <NavLink to={`/category/Psicología`} className={({ isActive }) => isActive ? 'text-violet-600' : 'text-black'}>Psicología</NavLink>
+                </div>
             </div>
 
 
