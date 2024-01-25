e1fc39a8b39503a3aae8456af21590a4c3aed28c
Alejandra Garcia <alejandragybra@gmail.com>
2024-01-24 21:14:52 -0300

    estilos NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
 
     return (
         <nav className='shadow-lg'>
-            <div className='flex items-center justify-between pr-14 shadow-lg bg-violet-600 md:pr-6'>
+            <div className='flex items-center justify-between pr-6 shadow-lg bg-violet-600'>
                 <h1 className='hidden'>Book-store</h1>
                 <div className='flex items-center gap-10'>
                     <Link to='/' className='flex justify-between items-center  pl-4 pr-4 pt-4 pb-4 rounded-r-3xl bg-white lg:pr-12 lg:pl-12'>
